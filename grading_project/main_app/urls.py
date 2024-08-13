3098b0e540f7cefebc8199c310afd419367015a1
256bit.by <deltas795@gmail.com>
2024-08-13 16:07:32 +0300

    sign-up forms

--- grading_project/main_app/urls.py
+++ grading_project/main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home_page, name='home'),
     path('home', views.home_page, name='home'),
+    path('sign-up', views.sign_up_page, name='sign-up'),
 ]
--- grading_project/main_app/views.py
+++ grading_project/main_app/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
+from . import forms
 
 
 def home_page(request):
     return render(request, 'main/home.html')
 
 
-#def sign_up_page(request):
-#    if request.method == 'POST' :
-#        form = RegisterForm(request.POST)
-#    else:
-#        form = RegisterForm()
-#
-#    return render(request, )
+def sign_up_page(request):
+    if request.method == 'POST':
+        form = forms.RegisterForm(request.POST)
+    else:
+        form = forms.RegisterForm()
+
+    return render(request, 'registration/sign_up.html', {"form": form})

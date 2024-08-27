791071588260aedbdf6a622aa10eb973ebafe34a
Никита Горчинский <92267602+Pandem1r@users.noreply.github.com>
2024-08-27 11:00:26 +0300

    Removed more cache from index. Add button for generate report in Profile

--- grading_project/main_app/admin.py
+++ grading_project/main_app/admin.py
@@ -1,16 +1,38 @@
 from django.contrib import admin
-
+from django.urls import path
+from django.shortcuts import render
+from django.utils.html import format_html
+from django.urls import reverse
 from main_app.models import *
 
 
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ['full_name', 'position', 'ratings']
+    list_display = ['full_name', 'position', 'ratings', 'get_report_button']
     search_fields = ['user__first_name', 'user__last_name', 'position__position_name', 'ratings']
 
     def full_name(self, obj):
         return obj.user.get_full_name()
-
     full_name.short_description = 'Юзер'
+
+    def get_report_button(self, obj):
+        return format_html('<a class="button" href="{}">Сформировать отчет</a>', reverse('admin:user_grading_report', args=[obj.pk]))
+    get_report_button.short_description = 'Отчет'
+
+    def get_urls(self):
+        urls = super().get_urls()
+        custom_urls = [
+            path('report/<int:profile_id>/', self.admin_site.admin_view(self.user_grading_report), name='user_grading_report'),
+        ]
+        return custom_urls + urls
+
+    def user_grading_report(self, request, profile_id):
+        profile = self.get_object(request, profile_id)
+        grading_records = Grading.objects.filter(user=profile.user)
+
+        return render(request, 'admin/user_grading_report.html', {
+            'profile': profile,
+            'grading_records': grading_records,
+        })
 
 
 class CriteriaAdmin(admin.ModelAdmin):
